75acfa92c631459d114e036542d6e3a6729cb05e
lukasnikolov <lukasnikolov@gmail.com>
2025-04-20 00:14:54 +0200

    app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def generate():
     return send_file(tmp.name, as_attachment=True, download_name="Rekapitulace_firma.docx")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0", port=10000)
